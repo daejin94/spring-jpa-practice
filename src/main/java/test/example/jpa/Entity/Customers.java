8f257b27838eb75286e9f2b52b0255b62354b77f
= <eowls1358@gmail.com>
2025-01-16 23:28:42 +0900

    feat: @Entity 어노테이션을 통한 ORM 설정 및 테이블 자동 생성

---
+++ src/main/java/test/example/jpa/Entity/Customers.java
@@ -0,0 +1,42 @@
+package test.example.jpa.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long customerNumber;
+
+    @Column(length = 50)
+    String customerName;
+
+    String contactLastName;
+
+    String contactFirstName;
+    String phone;
+
+    String addressLine1;
+    String addressLine2;
+
+    String city;
+
+    String state;
+
+    String postalCode;
+
+    String country;
+
+    Long creditLimit;
+
+}
