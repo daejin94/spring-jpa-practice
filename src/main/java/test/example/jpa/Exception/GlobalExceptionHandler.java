eb913abc6e9ee71d406d4d6d9479cc227746d7b8
= <eowls1358@gmail.com>
2025-05-14 17:16:37 +0900

    feat: - GlobalExceptionHandler 에 RuntimeException, Exception Class 핸들러 추가

--- src/main/java/test/example/jpa/Exception/GlobalExceptionHandler.java
+++ src/main/java/test/example/jpa/Exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package test.example.jpa.Exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.event.Level;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,6 +19,23 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(errorType.getStatus())
                 .body("잘못된 접근 입니다 : " + errorType.getMessage() + e.getDesc());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        log.atLevel(Level.ERROR).setCause(e).log(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Failed Program Error! Unexpected RuntimeException");
+    }
+
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(Exception e) {
+        log.atLevel(Level.ERROR).setCause(e).log(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Failed Program Error! Unexpected Exception");
     }
 
 }
